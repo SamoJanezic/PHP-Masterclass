4f292b10479f309b36793c2e469da3af134212ec
SamoJanezic <janezic.orion@gmail.com>
2022-05-17 18:17:32 +0200

    without styling

--- agiledrop-php-masterclass-helpers-91a6145518f6/article.php
+++ agiledrop-php-masterclass-helpers-91a6145518f6/article.php
@@ -1,7 +1,70 @@
 <?php
 include('helpers/posts.php');
 
-echo intval($_GET['ID']);
+
+$id = $_GET['ID'];
+if (!is_numeric($id) || $id > count($posts)-1) {
+    echo "Error: 404, Page not found";
+    die();
+} else {
+    $id = intval($_GET['ID']);
+}
+
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+  <title>Proceduralni CMS - Agiledrop PHP-Masterclass</title>
+  <!-- Favicon-->
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="css/styles.css" rel="stylesheet"/>
+</head>
+<body>
+<!-- Responsive navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Agiledrop PHP-Masterclass</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+        class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Page content-->
+<div class="container">
+  <div class="row"></div>
+  <?php
+
+    echo '<div><img src="'. $posts[$id]['image']['url'] . '" . alt="'. $posts[$id]['image']['alt'] . '"></div>';
+
+    echo "<p>" . $posts[$id]['title'] . "</p>";
+
+    echo "<p>" . $posts[$id]['content'] . "</p>";
+
+    echo "<p>" . $posts[$id]['authored by'] . "</p>";
+
+    echo "<p>" . date("d, m, Y", $posts[$id]['authored on']) . "</p>";
+
+    echo '<a href=' . "article.php?ID=$id". '>' . 'Preberi Več' . '</a>';
+
+  ?>
+  </div>
+</div>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
